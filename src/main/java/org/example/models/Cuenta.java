d048f1d3c945ee978827488ec0beac4ace46ec6f
vale <vale.cambria@hotmail.com>
2023-06-23 13:00:26 -0300

    agregando nueva practica

---
+++ src/main/java/org/example/models/Cuenta.java
@@ -0,0 +1,29 @@
+package org.example.models;
+
+import java.math.BigDecimal;
+
+public class Cuenta {
+    private String persona;
+    private BigDecimal saldo;
+
+    public Cuenta(String persona, BigDecimal saldo) {
+        this.saldo = saldo;
+        this.persona = persona;
+    }
+
+    public String getPersona() {
+        return persona;
+    }
+
+    public void setPersona(String persona) {
+        this.persona = persona;
+    }
+
+    public BigDecimal getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(BigDecimal saldo) {
+        this.saldo = saldo;
+    }
+}
---
+++ src/test/java/org/example/models/CuentaTest.java
@@ -0,0 +1,28 @@
+package org.example.models;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CuentaTest {
+
+    @Test
+    void testNombreCuenta() {
+        Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
+        //cuenta.setPersona("Andres");
+        String esperado = "Andres";
+        String actual = cuenta.getPersona();
+        assertEquals(esperado, actual);
+        assertTrue(actual.equals("Andres"));
+    }
+
+    @Test
+    void testSaldoCuenta(){
+        Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
+        assertEquals(1000.12345, cuenta.getSaldo().doubleValue());
+        assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);
+    }
+
+}
