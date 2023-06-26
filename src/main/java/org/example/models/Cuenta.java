07941be2a824ffe0f56fad85952c8672259420bb
vale <vale.cambria@hotmail.com>
2023-06-26 16:31:14 -0300

    agregando nuevo ejemplo

--- src/main/java/org/example/models/Cuenta.java
+++ src/main/java/org/example/models/Cuenta.java
@@ -26,4 +26,24 @@ public class Cuenta {
     public void setSaldo(BigDecimal saldo) {
         this.saldo = saldo;
     }
+
+    public void debito(BigDecimal monto){
+        this.saldo = this.saldo.subtract(monto); //un big decimal devuelve una nueva instancia con ese cambio, por eso hay que asignarle a this.saldo la resta
+    }
+
+    public void credito(BigDecimal monto){
+        this.saldo = this.saldo.add(monto);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Cuenta)){
+            return false;
+        }
+        Cuenta c = (Cuenta) obj;
+        if(this.persona == null || this.saldo == null){
+            return false;
+        }
+        return this.persona.equals(c.getPersona()) && this.saldo.equals(c.getSaldo());
+    }
 }
--- src/test/java/org/example/models/CuentaTest.java
+++ src/test/java/org/example/models/CuentaTest.java
@@ -14,15 +14,42 @@ class CuentaTest {
         //cuenta.setPersona("Andres");
         String esperado = "Andres";
         String actual = cuenta.getPersona();
+        assertNotNull(actual);
         assertEquals(esperado, actual);
-        assertTrue(actual.equals("Andres"));
+        assertEquals("Andres", actual);
     }
 
     @Test
     void testSaldoCuenta(){
         Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
+        assertNotNull(cuenta.getSaldo());
         assertEquals(1000.12345, cuenta.getSaldo().doubleValue());
         assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);
+        assertTrue(cuenta.getSaldo().compareTo(BigDecimal.ZERO) > 0);
     }
 
+    @Test
+    void testReferenciaCuenta(){
+        Cuenta cuenta = new Cuenta("John Doe", new BigDecimal("8900.9997"));
+        Cuenta cuenta2 = new Cuenta("John Doe", new BigDecimal("8900.9997"));
+        assertEquals(cuenta, cuenta2);
+        //assertNotEquals(cuenta, cuenta2);
+    }
+
+    @Test
+    void testDebitoCuenta() {
+        Cuenta cuenta = new Cuenta("Valentino", new BigDecimal("1000.12345"));
+        cuenta.debito(new BigDecimal(100));
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(900, cuenta.getSaldo().intValue());
+        assertEquals("900.12345", cuenta.getSaldo().toPlainString());
+    }
+    @Test
+    void testCreditoCuenta() {
+        Cuenta cuenta = new Cuenta("Valentino", new BigDecimal("1000.12345"));
+        cuenta.credito(new BigDecimal(100));
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(1100, cuenta.getSaldo().intValue());
+        assertEquals("1100.12345", cuenta.getSaldo().toPlainString());
+    }
 }
